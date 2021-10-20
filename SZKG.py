dc4107eccb82895bc17f8e1864d63d12fe6bcd34
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-10-20 14:19:22 +0200

    Add files via upload

---
+++ SZKG.py
@@ -0,0 +1,7 @@
+a = int(input('Addjon meg egy számot: '))
+b = int(input('Most egy másikat: '))
+
+print('A két szám összege: ' ,a+b)
+print('A két szám külömbsége: ' ,a-b)
+print('A két szám szorzata: ' ,a*b)
+print('A két szám hányadosa: ' ,a/b)
