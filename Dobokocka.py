67230c609063013db29671e86da040ecc01fe580
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-11-03 14:42:05 +0100

    Add files via upload

---
+++ Dobokocka.py
@@ -0,0 +1,32 @@
+import random
+a1=random.randrange(1,7)
+if a1==1:
+    print(a1)
+    print('...')
+    print('.*.')
+    print('...')
+elif a1==2:
+    print(a1)
+    print('...')
+    print('*.*')
+    print('...')
+elif a1==3:
+    print(a1)
+    print('*..')
+    print('.*.')
+    print('..*')
+elif a1==4:
+    print(a1)
+    print('*.*')
+    print('...')
+    print('*.*')
+elif a1==5:
+    print(a1)
+    print('*.*')
+    print('.*.')
+    print('*.*')
+else:
+    print(a1)
+    print('***')
+    print('...')
+    print('***')
