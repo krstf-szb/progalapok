e60909c0917467c6613211a9023f803b25a06525
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-11-10 14:32:09 +0100

    Add files via upload

---
+++ Doga.py
@@ -0,0 +1,51 @@
+print('1. Vers')
+
+print('Lejött a hegyről, majd visszament, ')
+print('félszegen káromkodott is csöppnyit,') 
+print('hát semmi sem patent,') 
+print('lesz tán pár öntelt halott...')
+
+
+print('2. Adatok')
+
+x = input('Név: ')
+y = int(input('Születési év: '))
+z = int(input('Születési hó: '))
+w = int(input('Születési nap: '))
+r = input('Születési hely: ')
+t = input('Lakcím: ')
+
+print('név: ' ,x)
+print('Év: ' ,y)
+print('Hó: ' ,z)
+print('Nap: ' ,w)
+print('Hely: ' ,r)
+print('Cím: ' ,t)
+
+
+print('3. számok')
+
+a = 69
+b = 420
+
+print(a,b)
+print(b,a)
+
+
+print('4.kerület terület')
+
+c = int(input('Szám 1: '))
+d = int(input('Szám 2: '))
+
+print('Téglalap területe:' ,a*b)
+print('Téglalap kerülete:' ,2*(a+b))
+
+
+print('5. 3szög')
+
+oldal1 = int(input('oldal 1: '))
+oldal2 = int(input('oldal 2: '))
+oldal3 = int(input('oldal 3: '))
+
+print('területe: ' ,oldal1*oldal2/2)
+print('kerülete: ' ,oldal1+oldal2+oldal3)
